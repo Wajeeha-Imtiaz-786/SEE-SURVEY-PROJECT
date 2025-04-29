e21d37dc241b47ff3ddac28bfd5b8a65fd591372
TALAA <tamer.alaaeldine@gmail.com>
2025-04-29 13:11:58 +0300

    adding PORT

--- database.py
+++ database.py
@@ -7,10 +7,12 @@ from sqlalchemy.orm import sessionmaker
 DB_USERNAME = os.getenv("DB_USERNAME", "survey_user")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "Islamabad123#")
 DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")  # Default to 3306 for MySQL
 DB_NAME = os.getenv("DB_NAME", "see_survey_db")
 
-DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+#DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
+DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 # Create engine and session
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
